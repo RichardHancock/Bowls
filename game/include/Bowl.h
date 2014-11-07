b4f2f118fc04e8aeb5583f5328239ff6db054313
Richard Hancock <me@richardhancock.co.uk>
2014-11-07 11:13:17 +0000

    More testing of doxygen in the Bowl.h file. (Format all header files like Bowl.h).

--- game/include/Bowl.h
+++ game/include/Bowl.h
@@ -2,24 +2,41 @@
 #pragma once
 #include "Ball.h"
 
-/// /class Bowl
-/// Defines a Bowl
+/** 
+@brief Class that represents a bowl
 
+Class that contains a bowl and its interactions with the game.
+*/
 class Bowl : public Ball
 {
 public:
 
-	/// /param transform The transformation matrix
+	/**
+	Create a Bowl object
+
+	Creates a Bowl object using the passed in transform. Colour and radius are set to internal defaults.
+
+	@param transform The transformation matrix
+	*/
 	Bowl(cgg::Mat43);
 
-	/// /param transform The transformation matrix
-	/// /param colour    Vec3 containing the r,g,b values
-	/// /param radius    Float value containing the radius of the sphere
+	/** 
+	Create a Bowl object
+
+	Creates a Bowl object using the passed in transform, colour and radius.
+
+	@param transform - The transformation matrix
+	@param colour - Vec3 containing the r,g,b values
+	@param radius - Float value containing the radius of the sphere
+	*/
 	Bowl(cgg::Mat43, cgg::Vec3, float);
 
-	/// /brief Renders a sphere
-	/// /param primitiveList*  Pointer to a list of primitives
-	/// 
-	/// Creates a sphere using the objects internal values and passes it into primitiveList.
+	/** 
+	Renders a sphere
+	
+	Creates a sphere using the objects internal values and passes it into primitiveList.
+
+	@param primitiveList* - Pointer to a list of primitives
+	*/
 	void render(gl::Primitives*);
 };
